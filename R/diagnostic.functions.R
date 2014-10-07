ce8ceb2d1fbdc45e3f43f69f79657d77858bc7de
Shawn P. Serbin <sserbin@bnl.gov>
2014-10-07 16:07:13 -0400

    Updates to code base.  Developing prototype code.  A work in progress.

--- R/diagnostic.functions.R
+++ R/diagnostic.functions.R
@@ -20,8 +20,196 @@
 
 #--------------------------------------------------------------------------------------------------#
 ##'
+##' A work in progress !! NEEDS UPDATING !!
 ##'
-##'
+##' GE Diagnostic plots 
+##' @param type which type of data to invert. A-Ci or A-Q
+##' @param data A-Ci data used for model fit
+##' @param DEoptim.output DEoptim output structure
+##' @param params 
+##' @param outdir output directory for A-Ci diagnostic figures
+##' @param file output filename
+##' @param f.model full, vcmax, or jmax
+##' 
+##' @author Shawn P. Serbin
+##' 
+plot.ge.fit <- function(type=c("A-Ci","A-Q"),data,DEoptim.output,params,outdir,file,f.model){
+  type <- match.arg(type)
+  sep <- .Platform$file.sep
+  param.length <- length(DEoptim.output$optim$bestmem)
+  
+  ### Plot params
+  cexaxis <- 1.2
+  cexlab <- 1.4
+  
+  if (type=="A-Ci"){
+    loc1 <- match(c("CI","PHOTO"),toupper(names(data)))
+    
+    # Figures
+    pdf(paste(outdir,sep,file,".pdf",sep=""),height=8,width=10)
+    plot(data[,loc1[1]],data[,loc1[2]],pch=21,bg="grey70",cex=3,cex.axis=cexaxis,xlim=c(0,range(data[,loc1[1]])[2]),
+         ylim=c(0,range(data[,loc1[2]])[2]),cex.lab=cexlab,xlab="Ci",ylab="Photo",main=paste(file))
+    box(lwd=2.2)
+    
+    Oxygen <- mm.constants$Oxygen
+    loc2 <- match(c("KC","KO","GSTAR","TLEAF"),toupper(names(data)))
+    Kc <- mean(data[,loc2[1]])
+    Ko <- mean(data[,loc2[2]])
+    Gstar <- mean(data[,loc2[3]])
+    
+    plot.x <- (data[,loc1[1]]-Gstar)/(data[,loc1[1]]+(Kc*(1+Oxygen/Ko)))
+    plot(plot.x,data[,loc1[2]],pch=21,bg="grey70",cex=3,cex.axis=cexaxis,xlim=c(0,range(plot.x)[2]),
+         ylim=c(0,range(data[,loc1[2]])[2]),cex.lab=cexlab,xlab="Ci-Gstar/Ci+Km",ylab="Photo",main=paste(file))
+    box(lwd=2.2)
+    
+    # DEoptim trace plots
+    if (f.model==1){
+      par(mfrow=c(3,1),mar=c(4,4.1,1,2)) #b, l, t, r
+      plot(DEoptim.output$member$bestmemit[,1],pch=21,bg="dark grey",col="dark grey",
+           cex=1.2,xlab="Iteration",ylab="Vcmax",cex.axis=cexaxis,cex.lab=cexlab)
+      lines(DEoptim.output$member$bestmemit[,1],lty=2,lwd=1.8)
+      box(lwd=2.2)
+      plot(DEoptim.output$member$bestmemit[,2],pch=21,bg="dark grey",col="dark grey",
+           cex=1.2,xlab="Iteration",ylab="Rd",cex.axis=cexaxis,cex.lab=cexlab)
+      lines(DEoptim.output$member$bestmemit[,2],lty=2,lwd=1.8)
+      box(lwd=2.2)
+      plot(DEoptim.output$member$bestmemit[,3],pch=21,bg="dark grey",col="dark grey",
+           cex=1.2,xlab="Iteration",ylab="Jmax",cex.axis=cexaxis,cex.lab=cexlab)
+      lines(DEoptim.output$member$bestmemit[,3],lty=2,lwd=1.8)
+      box(lwd=2.2)
+    } else if (f.model==2){
+      par(mfrow=c(3,1),mar=c(4,4.1,1,2)) #b, l, t, r
+      plot(DEoptim.output$member$bestmemit[,1],pch=21,bg="dark grey",col="dark grey",
+           cex=1.2,xlab="Iteration",ylab="Vcmax",cex.axis=cexaxis,cex.lab=cexlab)
+      lines(DEoptim.output$member$bestmemit[,1],lty=2,lwd=1.8)
+      box(lwd=2.2)
+      plot(DEoptim.output$member$bestmemit[,2],pch=21,bg="dark grey",col="dark grey",
+           cex=1.2,xlab="Iteration",ylab="Rd",cex.axis=cexaxis,cex.lab=cexlab)
+      lines(DEoptim.output$member$bestmemit[,2],lty=2,lwd=1.8)
+      box(lwd=2.2)
+    } else if (f.model==3){
+      par(mfrow=c(3,1),mar=c(4,4.1,1,2)) #b, l, t, r
+      plot(DEoptim.output$member$bestmemit[,1],pch=21,bg="dark grey",col="dark grey",
+           cex=1.2,xlab="Iteration",ylab="Jmax",cex.axis=cexaxis,cex.lab=cexlab)
+      lines(DEoptim.output$member$bestmemit[,1],lty=2,lwd=1.8)
+      box(lwd=2.2)
+      plot(DEoptim.output$member$bestmemit[,2],pch=21,bg="dark grey",col="dark grey",
+           cex=1.2,xlab="Iteration",ylab="Rd",cex.axis=cexaxis,cex.lab=cexlab)
+      lines(DEoptim.output$member$bestmemit[,2],lty=2,lwd=1.8)
+      box(lwd=2.2)
+    }
+    
+    # A-Ci diagnostic fit fig
+    plotCi = seq(-2,2200,2)
+    Oxygen <- mm.constants$Oxygen
+    loc2 <- match(c("KC","KO","GSTAR","TLEAF"),toupper(names(data)))
+    Kc <- mean(data[,loc2[1]])
+    Ko <- mean(data[,loc2[2]])
+    Gstar <- mean(data[,loc2[3]])
+    Tleaf <- mean(data[,loc2[4]])
+    Vcmax.plot <- params[[1]]
+    Jmax.plot <- params[[2]]
+    Rd.plot <-params[[3]]
+    RMSE.plot <- params[[4]]
+    
+    par(mfrow=c(1,1),mar=c(5,5,2,1)) # bot, left
+    ylim <- range(data[,loc1[2]])
+    plot(data[,loc1[1]],data[,loc1[2]], main=paste(file), xlab="Ci", ylab="Photo", cex.lab=2,cex=1.8,
+         xlim=c(1.1,2000),ylim=c(0,ylim[2]+3))
+    legend("bottomright",legend=c(paste("Tleaf =",round(Tleaf,2)),paste("Vcmax =",round(Vcmax.plot,2)),
+                                  paste("Jmax = ",round(Jmax.plot,2)),paste("Rd = ",round(Rd.plot,4)),
+                                  paste("RMSE = ",round(RMSE.plot,2))),
+           bty="n",cex=2)
+    legend("topleft",legend=c("Rubisco","RuBP","Photo"),lty=c(2,2,1),
+           col=c("dark blue","dark red","dark grey"),bty="n",lwd=6.3,seg.len=3.5,cex=1.5)
+    if (Vcmax.plot!=-9999){
+      lines(plotCi,(Vcmax.plot*(plotCi-Gstar)/(plotCi+(Kc*(1+Oxygen/Ko))))-Rd.plot,lwd=5,col="dark blue",lty=2)
+    }
+    if (Jmax.plot!=-9999){
+      lines(plotCi,((Jmax.plot*(plotCi-Gstar))/((4.5*plotCi)+(10.5*Gstar)))-Rd.plot,lwd=5,col="dark red",lty=2)
+    }
+    if (Vcmax.plot!=-9999 & Jmax.plot!=-9999){
+      lines(plotCi,pmin(Vcmax.plot*(plotCi-Gstar)/(plotCi+(Kc*(1+Oxygen/Ko))),
+                        (Jmax.plot*(plotCi-Gstar))/((4.5*plotCi)+(10.5*Gstar)))-Rd.plot,col="dark grey",lwd=2.0)
+    }
+    box(lwd=2.2)
+    dev.off()
+    
+    
+  } else if (type=="A-Q"){
+    
+    loc1 <- match(c("PARI","PHOTO"),toupper(names(data)))
+    
+    # Figures
+    pdf(paste(outdir,sep,file,".pdf",sep=""),height=8,width=10)
+    plot(data[,loc1[1]],data[,loc1[2]],pch=21,bg="grey70",cex=3,cex.axis=cexaxis,xlim=c(0,range(data[,loc1[1]])[2]),
+         ylim=c(0,range(data[,loc1[2]])[2]+1),cex.lab=cexlab,xlab="PARi",ylab="Photo",main=paste(file))
+    box(lwd=2.2)
+    
+    # DEoptim trace plots
+    if (f.model==1){
+      par(mfrow=c(2,1),mar=c(4,4.1,1,2)) #b, l, t, r
+      plot(DEoptim.output$member$bestmemit[,1],pch=21,bg="dark grey",col="dark grey",
+           cex=1.2,xlab="Iteration",ylab="Amax",cex.axis=cexaxis,cex.lab=cexlab)
+      lines(DEoptim.output$member$bestmemit[,1],lty=2,lwd=1.8)
+      box(lwd=2.2)
+      plot(DEoptim.output$member$bestmemit[,2],pch=21,bg="dark grey",col="dark grey",
+           cex=1.2,xlab="Iteration",ylab="Convex",cex.axis=cexaxis,cex.lab=cexlab)
+      lines(DEoptim.output$member$bestmemit[,2],lty=2,lwd=1.8)
+      box(lwd=2.2)
+    }
+    
+    # A-Q diagnostic fit fig
+    plotPARi <- seq(-5,2000,2)
+    loc2 <- match(c("PARI","TLEAF"),toupper(names(data)))
+    PARi <- data[,loc2[1]]
+    Tleaf <- mean(data[,loc2[2]])
+    aQY.plot <- params[[1]]   
+    LCP.plot <- params[[2]]
+    Amax.plot <- params[[3]]
+    convex.plot <- params[[4]]
+    LUE.plot <- params[[5]]
+    Rd.plot <- params[[6]]
+    RMSE.plot <- params[[7]]
+    IaQY <- aQY.plot*plotPARi
+    
+    par(mfrow=c(1,1),mar=c(5,5,2,1)) # bot, left
+    ylim <- range(data[,loc1[2]])
+    plot(data[,loc1[1]],data[,loc1[2]], main=paste(file), xlab="PARi", ylab="Photo", cex.lab=2,cex=3,
+         xlim=c(1.1,1500),ylim=c(ylim[1],ylim[2]+3))
+    legend("bottomright",legend=c(paste("Tleaf =",round(Tleaf,2)),paste("LCP =",round(LCP.plot,2)),
+                                  paste("aQY =",round(aQY.plot,3)),paste("Amax = ",round(Amax.plot,2)),
+                                  paste("Convex = ",round(convex.plot,2)),paste("LUE.max = ",round(LUE.plot,4)),
+                                  paste("Rd = ",round(Rd.plot,4)),
+                                  paste("RMSE = ",round(RMSE.plot,2))),bty="n",cex=2)
+    legend("topleft",legend=c("aQY","Photo"),lty=c(2,1),
+           col=c("dark blue","dark grey"),bty="n",lwd=6.3,seg.len=3.5,cex=1.5)
+    box(lwd=2.2)
+    
+    if (Amax.plot!=-9999){
+      lines(plotPARi[plotPARi<=100],aQY.plot*plotPARi[plotPARi<=100] - Rd.plot,lwd=5,col="dark blue",lty=2)
+      lines(plotPARi,(((IaQY+Amax.plot)-sqrt((IaQY+Amax.plot)^2-(4*convex.plot*IaQY*Amax.plot)))/(2*convex.plot))-Rd.plot,
+            col="dark grey",lwd=2.0)
+      box(lwd=2.2)
+      
+      # LUE plot
+      plotPARi <- seq(0,2000,1)
+      IaQY <- aQY.plot*plotPARi
+      plot(plotPARi,((((IaQY+Amax.plot)-sqrt((IaQY+Amax.plot)^2-(4*convex.plot*IaQY*Amax.plot)))/(2*convex.plot))-Rd.plot)
+           /plotPARi,type="l",ylab="LUE",xlab="PARi",ylim=c(0,0.1),lwd=3)
+      points(which.max(((((IaQY+Amax.plot)-sqrt((IaQY+Amax.plot)^2-(4*convex.plot*IaQY*Amax.plot)))/(2*convex.plot))-Rd.plot)
+                       /plotPARi),max(((((IaQY+Amax.plot)-sqrt((IaQY+Amax.plot)^2-(4*convex.plot*IaQY*Amax.plot)))/(2*convex.plot))-Rd.plot)
+                                      /plotPARi),pch=20,bg="dark grey",cex=3.3)
+      legend("topleft",legend="A/PARi",col="black",bty="n",lwd=6.3,seg.len=3.5,cex=1.5)
+      legend("topright",legend=paste("LUE.max = ",round(LUE.plot,4)),,bty="n",cex=2)
+      box(lwd=2.2)
+    } else {
+      lines(plotPARi[plotPARi<=100],aQY.plot*plotPARi[plotPARi<=100] - Rd.plot,lwd=5,col="dark blue",lty=2)
+    }
+    dev.off()
+  } # End A-Ci / A-Q if/else
+  
+} # End of function
 #==================================================================================================#
 
 
