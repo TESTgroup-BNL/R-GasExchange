18e808ea1170e41ac40499535c07921d20d34e90
Shawn P. Serbin <sserbin@bnl.gov>
2014-10-07 14:25:04 -0400

    Minor update

--- inst/install.dependencies.R
+++ inst/install.dependencies.R
@@ -1,4 +1,8 @@
-list.of.packages <- c('XML','testthat') 
+#!/usr/bin/env Rscript
+#
+# Install required packages for R-GasExchange
+#
+list.of.packages <- c('XML','DEoptim') 
 
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) {
@@ -7,3 +11,13 @@ if(length(new.packages)) {
   install.packages(new.packages, repos="http://cran.us.r-project.org")
 }
 
+# Look for suggested packages
+if(!('testthat') %in% installed.packages()[,"Package"]) {
+  print("Suggested install testthat not installed")
+  print("You can install the driver using the following command in R.")
+  print("install.packages('testthat', repos='http://cran.rstudio.com/')")
+} else if (!('devtools') %in% installed.packages()[,"Package"]) {
+  print("Suggested install devtools not installed")
+  print("You can install the driver using the following command in R.")
+  print("install.packages('devtools', repos='http://cran.rstudio.com/')")
+}
