30852f5e6bbcc3d1ee2f6447adc0802c43b62d1d
Shawn P. Serbin <sserbin@bnl.gov>
2014-10-07 15:08:29 -0400

    Updates to code base.  Developing prototype code.  A work in progress.

--- R/photosynthesis.functions.R
+++ R/photosynthesis.functions.R
@@ -42,11 +42,50 @@ ACi.rubp  <- function(x) {
 #==================================================================================================#
 
 
+#--------------------------------------------------------------------------------------------------#
+# Vcmax, Rd, and Jmax
+ACi.full <- function(x) {
+  Vcmax <- x[1]  ## Vcmax param
+  Rd    <- x[2]  ## Resp param
+  Jmax  <- x[3]  ## Jmax
+  
+  Ac <- ifelse(Ci<=Vcmax.cutoff,(Vcmax*(Ci-Gstar)/(Ci + Km))-Rd,9999)
+  inter <- ifelse((Ci>Vcmax.cutoff & Ci<Jmax.cutoff),Photo,9999)
+  Aj <- ifelse(Ci>=Jmax.cutoff,(Jmax*(Ci-Gstar)/((4.5*Ci)+(10.5*Gstar)))-Rd,9999)
+  #Aj <- ifelse(Ci>=Jmax.cutoff,(Jmax*(Ci-Gstar)/((4.5*Ci)+(10.5*Gstar))),9999)
+  Assim <- pmin(Ac,Aj,inter)
+  RMSE <- sqrt(mean((Photo-Assim)^2))  ## RMSE cost function
+  return(RMSE)
+}
+#==================================================================================================#
+
+
 # ============================================== C4 ================================================ 
 
 
 #--------------------------------------------------------------------------------------------------#
 ##' 
+#==================================================================================================#
+
+
+# ========================================= Light Response ========================================= 
+
+
+#--------------------------------------------------------------------------------------------------#
+# A-PPFD parameters
+A.Q <- function(x) { 
+  ## Setup variables to solve for
+  Amax  <- x[1]  # Amax param.  Light saturated rate of photo
+  convex <- x[2]  # Convexity of the light response curve
+  
+  # Rectangular hyper
+  #mod.Photo <- ((IaQY+Amax)-sqrt((IaQY+Amax)^2-(4*convex*IaQY*Amax)))/(2*convex)
+  mod.Photo <- (((IaQY+Amax)-sqrt((IaQY+Amax)^2-(4*convex*IaQY*Amax)))/(2*convex))-Rd[i]
+  An <- mod.Photo
+  #An <- mod.Photo-Rd[i]
+  RMSE <- sqrt(mean((Photo-An)^2))  ## RMSE cost function
+  return(RMSE)
+} 
 #==================================================================================================#
 
 
